b41d67322a757553368d4f51025ee2d87686a3e2
komporbakar <laravelgg00@gmail.com>
2023-08-24 23:32:06 +0700

    update location image

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -14,7 +14,7 @@ function CardList({...props}) {
     <div className='w-full mb-5 lg:w-1/4 md:w-1/3 lg:px-2 md:px-2'>
             <div className="relative">
             <Link to={`/museum/${item}`}> 
-                <img src={`public/images/${src ?? 'default'}.jpg`} alt="" className='w-full h-60 rounded-lg mb-2 object-cover' />
+                <img src={`images/${src ?? 'default'}.jpg`} alt="" className='w-full h-60 rounded-lg mb-2 object-cover' />
             </Link>
                 <div onClick={clickLove} className='absolute bottom-0 right-0 bg-emerald-100 w-10 h-10 opacity-90  rounded-br-md rounded-tl-md hover:bg-button-1 hover:fill-font-1 cursor-pointer'>
                     <Love  className={`p-2 ${toggle ?  'bg-button-1 fill-font-1 rounded-br-md rounded-tl-md hover:bg-font-1 hover:fill-button-1'  : 'fill-button-1 hover:fill-font-1 '}`}/>
