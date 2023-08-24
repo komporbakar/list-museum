1b7a5c3cefb77344aa70603c6c52621d6241d07a
komporbakar <laravelgg00@gmail.com>
2023-08-24 23:33:50 +0700

    fixed image path

--- src/components/Atoms/ImgSweper.jsx
+++ src/components/Atoms/ImgSweper.jsx
@@ -4,7 +4,7 @@ import { SwiperSlide } from 'swiper/react';
 function ImgSweper({image}) {
   return (
         <>
-            <img src={`../../public/images/${image}.jpg`} className='w-full lg:min-h-96 object-cover' alt="" />
+            <img src={`images/${image}.jpg`} className='w-full lg:min-h-96 object-cover' alt="" />
         </>
      
     
